4901750cee15d784706679dd4052c6fec7a2138b
zhuolongli1-spec <319705251+zhuolongli1-spec@users.noreply.github.com>
2026-08-24 07:41:34 +0800

    更改站点描述

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -40,21 +40,21 @@ const pages = resolvePageToggles({
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "lzl",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "blog site",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://www.lzlblog.xyz",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"lzlblog 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客,记录前端学习笔记与日常生活",
 
 	// 站点关键词
 	keywords: [
-		"Firefly",
+		"lzlblog",
 		"Fuwari",
 		"Astro",
 		"ACGN",
@@ -127,7 +127,7 @@ export const siteConfig: SiteConfig = {
 	},
 
 	// 站点开始日期，用于统计运行天数
-	siteStartDate: "2025-01-01",
+	siteStartDate: "2026-08-22",
 
 	// 站点时区（IANA 时区字符串），用于格式化bangumi、rss里的构建日期时间等等..
 	// 示例："Asia/Shanghai", "UTC", 如果为空，则按照构建服务器的时区进行时区转换
