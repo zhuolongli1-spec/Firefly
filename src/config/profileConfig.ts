14bc82e024e4ef1e430d42e6101a495a67d02d2f
zhuolongli1-spec <319705251+zhuolongli1-spec@users.noreply.github.com>
2026-08-23 12:52:02 +0800

    修改个人信息

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -24,13 +24,13 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "qq",
 			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com/q/ZGsFa8qX2G",
+			url: "https://user.qzone.qq.com/1058709496",
 			showName: false,
 		},
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://github.com/zhuolongli1-spec",
 			showName: false,
 		},
 		{
