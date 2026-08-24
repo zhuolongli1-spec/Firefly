5065438e1a68275518ffff74e780abb48231909b
zhuolongli1-spec <319705251+zhuolongli1-spec@users.noreply.github.com>
2026-08-25 07:56:34 +0800

    修改主体颜色

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -66,7 +66,7 @@ export const siteConfig: SiteConfig = {
 	// 主题色
 	themeColor: {
 		// 主题色的默认色相，范围从 0 到 360。例如：红色：0，青色：200，蓝绿色：250，粉色：345
-		hue: 165,
+		hue: 220,
 		// 默认模式："light" 亮色，"dark" 暗色，"system" 跟随系统
 		defaultMode: "system",
 	},
