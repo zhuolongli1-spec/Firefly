be2b8f5547bbe588c237fc97b50a5a5649fce1f4
zhuolongli1-spec <319705251+zhuolongli1-spec@users.noreply.github.com>
2026-08-25 07:53:38 +0800

    修改主标题

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -4,7 +4,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 	// 壁纸模式："banner" 横幅壁纸，"fullscreen" 全屏壁纸，"overlay" 覆盖透明，"none" 纯色背景无壁纸
 	mode: "fullscreen",
 	// 是否启用背景视频播放，配置后将在导航栏显示视频播放按钮
-	playerEnable: true,
+	playerEnable: false,
 	/**
 	 * 背景图片配置
 	 * 图片路径支持三种格式：
