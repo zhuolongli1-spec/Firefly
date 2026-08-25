b1c3a51e8653276683277ddc79f89b125001905f
zhuolongli1-spec <319705251+zhuolongli1-spec@users.noreply.github.com>
2026-08-25 13:12:00 +0800

    把手机壁纸移到public文件夹

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -45,8 +45,8 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
 		mobile: [
-			"assets/images/MobileWallpaper/m1-v5.avif",
-			"assets/images/MobileWallpaper/m2-v5.avif",
+			"/assets/images/MobileWallpaper/m1-v5.avif",
+			"/assets/images/MobileWallpaper/m2-v5.avif",
 		],
 		// 背景视频播放地址
 		// 支持单个视频路径（字符串）或多个视频循环（数组，参考上面壁纸配置）
