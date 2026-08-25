4836a3855d4576dd0d7111d93f0ec33eb4c49f60
zhuolongli1-spec <319705251+zhuolongli1-spec@users.noreply.github.com>
2026-08-25 08:47:13 +0800

    修改logo

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -115,7 +115,7 @@ export const siteConfig: SiteConfig = {
 			alt: "🍀",
 		},
 		// 导航栏标题
-		title: "Firefly",
+		title: "lzlblog",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
